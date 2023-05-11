76a1f71badb5fad81d7bb975eea9a53dc8445dab
mluba <mluba@DESKTOP-CPTRK5M.ds.usfca.edu>
2023-05-11 10:04:03 -0700

    Updated documentation for the Node class

--- Record.java
+++ Record.java
@@ -6,13 +6,21 @@ public class Record {
 	String from = new String();
 	List<String> toList = new ArrayList<>();
 	
-	
+	/**
+	 * Constructor for a record.
+	 * 
+	 * @param from - "From" address.
+	 * @param emails - List of email address in contact with "From".
+	 */
 	public Record (String from, List<String> emails) {
 		this.from = from;
 		this.toList = emails;
 		
 	}
 	
+	/**
+	 * Prints the record.
+	 */
 	public String toString () {
 		return "From: " + this.from + "\nTo: " + this.toList;
 	}
