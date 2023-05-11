4ac57d4bc5355fc1d8b9f18ae56fb93fd4690b3b
mluba <mluba@DESKTOP-CPTRK5M.ds.usfca.edu>
2023-05-11 09:26:21 -0700

    Updated Node with addEmailsToReceive

--- Node.java
+++ Node.java
@@ -5,26 +5,45 @@ public class Node {
 
 	public String email = "";
 	public String name = "";
-	List<String> neighbors = new ArrayList<>();
+	List<String> sentEmails = new ArrayList<>();
+	List<String> receivedEmails = new ArrayList<>();
 	boolean visited = false;
+	int dfsNum = 0;
+	int back = 0;
 	
 	public Node (Record record) {
 		email = record.from;
-		addEmails(record);
+		addEmailsToSend(record);
+	}
+	
+	public Node (String email, String received) {
+		this.email = email;
+		this.receivedEmails.add(received);
 	}
 	
 	public String toString () {
-		return "\nEmail: " + this.email + " Name: " + this.name + "\nNeighbors: " + this.neighbors;
+		return "\nEmail: " + this.email + " Name: " + this.name + "\nNeighbors: " + this.sentEmails
+				+ "\nReceived: " + this.receivedEmails ;
+		
 	}
 
-	public void addEmails(Record record) {
+	public void addEmailsToSend(Record record) {
 		for (String toEmail : record.toList) {
 			if (this.email.equals(toEmail)) {
 				continue;
-			} else if (neighbors.contains(toEmail)) {
+			} else if (sentEmails.contains(toEmail)) {
 				continue;
 			}
-			neighbors.add(toEmail);
+			sentEmails.add(toEmail);
 		}
+	}
+	
+	public void addEmailsToReceive(Record record) {
+		if (this.email.equals(record.from)) {
+			return;
+		} else if (receivedEmails.contains(record.from)) {
+			return;
+		}
+		receivedEmails.add(record.from);
 	}
 }
