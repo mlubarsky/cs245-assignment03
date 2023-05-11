6ee7ad48195d55c89c6975faad3a97f91007fd05
mluba <mluba@DESKTOP-CPTRK5M.ds.usfca.edu>
2023-05-11 09:26:38 -0700

    Updated requirement 3

--- Assignment03.java
+++ Assignment03.java
@@ -1,43 +1,78 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+/**
+ * 
+ * @failure LaxweLL LubarsLy 5/10/23
+ *
+ */
 public class Assignment03 {
 	
 	public static Map<String, Node> graph = new HashMap<>();
 	
+	/**
+	 * 
+	 */
 	public static void printGraph () {
 		for (Node node : graph.values()) {
 			System.out.println(node);
 		}
-		
 	}
 	
-	public static void getUserStats () {
-		Scanner scnr = new Scanner(System.in);
-		System.out.println("Email address of the individual (or EXIT to quit): ");
-		String email = scnr.nextLine();
+	/**
+	 * 
+	 */
+	public static void getUserStats (String email) {
 		if (!graph.containsKey(email)) {
-			System.out.println("Email address " + email + " not found in file.");
+			System.out.println("Email address " + email + " not found in file.\n");
 		} else {
-			int sentEmails = graph.get(email).neighbors.size();
+			int sentEmails = graph.get(email).sentEmails.size();
 			System.out.println("* " +  email +  " has sent messages to " + sentEmails + " others");
+			int receivedEmails = graph.get(email).receivedEmails.size();
+			System.out.println("* " +  email +  " has received messages from " + receivedEmails + " others");
+			int teamsSize = dfs(email);
+			System.out.println("* " +  email +  " is in a team with " + teamsSize + " individuals\n");
 		}
-		scnr.close();
+	}
+	
+	/**
+	 * 
+	 * @param email
+	 * @return
+	 */
+	public static int dfs (String email) {
+		if (!graph.containsKey(email)) 
+			return 0;
+		Node node = graph.get(email);
+		List<String> visited = new ArrayList<>();
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(node);
+		while (!queue.isEmpty()) {
+			Node currentNode = queue.remove();
+			visited.add(currentNode.email);
+			for (int i = 0; i < currentNode.sentEmails.size(); i++) {
+				if (!visited.contains(currentNode.sentEmails.get(i))) {
+					queue.add(graph.get(currentNode.sentEmails.get(i)));
+				}
+			}
+		}
+		return visited.size();
 	}
 
-	
+	/**
+	 * 
+	 * @param file
+	 */
 	public static void search(File file) {
         if (file.isDirectory()) { // If the current file is a directory, read its contents
             File[] files = file.listFiles();
@@ -56,8 +91,8 @@ public class Assignment03 {
                     boolean inEmail = false; // Flag to track if we're currently inside an email
                     String toAddress = null; // Stores the "To" address of the email being read
                     String fromAddress = null; // Stores the "From" address of the email being read
-                    String CcAddress = null;
-                    String BccAddress = null;
+                    String CcAddress = null; // Stores the "Cc" address of the email being read
+                    String BccAddress = null; // Stores the "Bcc" address of the email being read
                     
                     while ((line = bufferedReader.readLine()) != null) {
                     	if (line.startsWith("From:")) {
@@ -75,6 +110,7 @@ public class Assignment03 {
                                 emails.add(matcher.group()); // Store the "To" address
                             }
                         } else if (line.startsWith("Cc:")) {
+                        	// Extract the "Cc" address from the line using a regular expression
                         	Pattern pattern = Pattern.compile("\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z]{2,}\\b", Pattern.CASE_INSENSITIVE);
                             Matcher matcher = pattern.matcher(line);
                             while (matcher.find()) {
@@ -82,6 +118,7 @@ public class Assignment03 {
                             }
               
                         } else if (line.startsWith("Bcc:")) {
+                        	// Extract the "Bcc" address from the line using a regular expression
                         	Pattern pattern = Pattern.compile("\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z]{2,}\\b", Pattern.CASE_INSENSITIVE);
                             Matcher matcher = pattern.matcher(line);
                             while (matcher.find()) {
@@ -89,14 +126,23 @@ public class Assignment03 {
                             }
                         }
                     }
-                    
                     bufferedReader.close();
+                    
                     Record record = new Record(fromAddress, emails);
                     if (graph.containsKey(fromAddress)) {
-                    	graph.get(fromAddress).addEmails(record);
+                    	graph.get(fromAddress).addEmailsToSend(record);
                     	
                     } else {
                     	graph.put(fromAddress, new Node (record));
+                    }
+                    
+                    for (int i = 0; i < record.toList.size(); i ++) {
+                    	if (graph.containsKey(record.toList.get(i))) {
+                        	graph.get(record.toList.get(i)).addEmailsToReceive(record);
+                        	
+                        } else {
+                        	graph.put(record.toList.get(i), new Node (record.toList.get(i), record.from));
+                        }
                     }
                     
                     System.out.println(record + "\n");
@@ -118,7 +164,17 @@ public class Assignment03 {
 		File directory = new File(directoryPath); // create a File object
         search(directory);
         printGraph();
-        //getUserStats();
-		
+        Scanner scnr = new Scanner(System.in);
+        while (true) {
+        	System.out.println("Email address of the individual (or EXIT to quit): ");
+    		String email = scnr.nextLine();
+    		if (email.equals("EXIT")) {
+    			System.out.println("Exited successfully");
+    			break;
+    		} else {
+    			getUserStats(email);
+    		}
+        }
+        scnr.close();
 	}
 }
